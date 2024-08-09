24fce367658e0a3789baa7fbf247050dc649d825
Ravelo-Dev <zmarcos140@gmail.com>
2024-08-09 14:07:33 -0400

    Actualizacion del proyecto:
    Se agregaron opciones como LogOut = signout
    LogIn = signin con sus urls.
    
    signin.html agregado

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, HttpResponse, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 from django.db import IntegrityError
 
 # Create your views here.
@@ -33,5 +33,13 @@ def signup(request):
 
 def tasks(request):
     return render(request, 'tasks.html')
+
+def signout(request):
+    logout(request)
+    return redirect('home')
+
+def signin(request):
+    return render(request, 'signin.html', {
+        'form': AuthenticationForm})
 
         
